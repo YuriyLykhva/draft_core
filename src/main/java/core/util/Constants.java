8c482b15166b389ddb5c6064d62485fb130fb9e9
YuriyLykhva <67342223+YuriyLykhva@users.noreply.github.com>
2023-04-20 10:16:10 +0300

    Update Constants.java

--- src/main/java/core/util/Constants.java
+++ src/main/java/core/util/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
     public static final String SIGNIN_PAGE_URL = "http://192.168.0.101:8080";
     public static final boolean USE_REMOTE_WEB_DRIVER = true;
     public static final String REMOTE_WEB_SERVER_URL = "http://192.168.0.101:4444";
-    public static final boolean USE_FULL_PAGE_SCREENSHOT = true;
+    public static final boolean USE_FULL_PAGE_SCREENSHOT = false;
 
     private Constants(){}
 
