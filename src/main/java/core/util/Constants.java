9a59041e95326dd68a63f82482a42aef1da3516d
YuriyLykhva <67342223+YuriyLykhva@users.noreply.github.com>
2023-05-23 11:31:29 +0300

    Update Constants.java

--- src/main/java/core/util/Constants.java
+++ src/main/java/core/util/Constants.java
@@ -3,9 +3,9 @@ package core.util;
 public class Constants {
 
     public static final int WAIT_TIMEOUT_SECONDS = 15;
-    public static final String SIGNIN_PAGE_URL = "http://192.168.0.101:8080";
+    public static final String SIGNIN_PAGE_URL = "http://localhost:8080";
     public static final boolean USE_REMOTE_WEB_DRIVER = false;
-    public static final String REMOTE_WEB_SERVER_URL = "http://192.168.0.101:4444";
+    public static final String REMOTE_WEB_SERVER_URL = "http://localhost:4444";
     public static final boolean USE_FULL_PAGE_SCREENSHOT = true;
 
     private Constants(){}
