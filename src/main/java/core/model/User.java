84540ca32d2aab46c731b990cde71ce68e9b4de5
YuriyLykhva <67342223+YuriyLykhva@users.noreply.github.com>
2023-05-29 17:28:29 +0300

    Update User.java

--- src/main/java/core/model/User.java
+++ src/main/java/core/model/User.java
@@ -11,28 +11,48 @@ import static core.util.EncoderDecoder.getEncodedValue;
 public class User {
     private String login;
     private String password;
+    private String token;
+    private String role;
+    private String userProject;
 
     public static User createUser() {
-        return new User(getTestData("login"), getTestData("password"));
+        return new User(
+                getTestData("login"),
+                getTestData("password"),
+                getTestData("token"),
+                getTestData("role"),
+                getTestData("userProject"));
     }
 
     private static final ResourceBundle resourceBundle =
-            ResourceBundle.getBundle("admin");
+//            ResourceBundle.getBundle("admin");
+            ResourceBundle.getBundle("user");
 
     public static String getTestData(String key) {
         return resourceBundle.getString(key);
     }
 
-    private User(String login, String password) {
+    private User(String login, String password, String token, String role, String userProject) {
         this.login = getEncodedValue(login);
         this.password = getEncodedValue(password);
+        this.token = getEncodedValue(token);
+        this.role = getEncodedValue(role);
+        this.userProject = getEncodedValue(userProject);
     }
 
     public String getLogin() {
         return getDecodedValue(login);
     }
-
     public String getPassword() {
         return getDecodedValue(password);
+    }
+    public String getToken() {
+        return getDecodedValue(token);
+    }
+    public String getRole() {
+        return getDecodedValue(role);
+    }
+    public String getUserProject() {
+        return getDecodedValue(userProject);
     }
 }
