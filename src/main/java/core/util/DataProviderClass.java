5d6d1d2bb342e3d85f51c1be8e754e497dcc2cbf
YuriyLykhva <67342223+YuriyLykhva@users.noreply.github.com>
2023-05-29 16:17:32 +0300

    Update DataProviderClass.java

--- src/main/java/core/util/DataProviderClass.java
+++ src/main/java/core/util/DataProviderClass.java
@@ -4,9 +4,6 @@ import org.testng.annotations.DataProvider;
 
 public class DataProviderClass {
 
-    /**
-     * This class is intended to transfer credentials from CSV-file into TA framework
-     */
     @DataProvider(name = "credentials")
     public Object[][] credentials() {
         return new String[][]{
