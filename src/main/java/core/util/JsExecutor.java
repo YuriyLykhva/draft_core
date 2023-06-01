b8305bd2d32122bdc853d072d2ddad3050553f77
Yurii_Lykhva <yuriy.lykhva@gmail.com>
2023-06-01 22:54:04 +0300

    fixed js executor

--- src/main/java/core/util/JsExecutor.java
+++ src/main/java/core/util/JsExecutor.java
@@ -1,19 +1,33 @@
 package core.util;
 
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import static core.driver.WebDriverFactory.getDriver;
 
 public class JsExecutor {
 
+    private static JavascriptExecutor jsExecutor;
+    private static WebDriver driver;
+
+    private JsExecutor() {}
+
+    public static synchronized JavascriptExecutor getInstance() {
+        if (jsExecutor == null) {
+            driver = getDriver();
+            jsExecutor = (JavascriptExecutor) driver;
+        }
+        return jsExecutor;
+    }
+
     protected void jsClickElement(WebElement element) {
-        JavascriptExecutor executor = (JavascriptExecutor) getDriver();
+        JavascriptExecutor executor = JsExecutor.getInstance();
         executor.executeScript("arguments[0].click();", element);
     }
 
     protected void jsViewElement(WebElement element) {
-        JavascriptExecutor executor = (JavascriptExecutor) getDriver();
+        JavascriptExecutor executor = JsExecutor.getInstance();
         executor.executeScript("arguments[0].scrollIntoView(true);", element);
     }
 }
